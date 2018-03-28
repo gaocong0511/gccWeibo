1d93d9657f6b938fb64682e3a61f35a4087a84ce
gaocong0511 <qq370010866gc@gmail.com>
2018-03-28 23:48:20 +0800

    每天都要写代码呀~ 处理图片浏览开始

--- app/src/main/java/com/nonk/gaocongdeweibo/BaseApplication.java
+++ app/src/main/java/com/nonk/gaocongdeweibo/BaseApplication.java
@@ -4,11 +4,13 @@ import android.app.Application;
 import android.content.Context;
 
 import com.nonk.gaocongdeweibo.Bean.User;
+import com.nonk.gaocongdeweibo.utils.GccImageLoader;
 import com.nonk.gaocongdeweibo.utils.ImageOptHelper;
 import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.previewlibrary.ZoomMediaLoader;
 
 
 public class BaseApplication extends Application {
@@ -20,6 +22,7 @@ public class BaseApplication extends Application {
     public void onCreate() {
         super.onCreate();
         initImageLoader(this);
+        ZoomMediaLoader.getInstance().init(new GccImageLoader());
     }
 
     private void initImageLoader(Context context){
---
+++ app/src/main/java/com/nonk/gaocongdeweibo/utils/GccImageLoader.java
@@ -0,0 +1,68 @@
+package com.nonk.gaocongdeweibo.utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.target.Target;
+import com.bumptech.glide.request.transition.Transition;
+import com.nonk.gaocongdeweibo.R;
+import com.previewlibrary.loader.IZoomMediaLoader;
+import com.previewlibrary.loader.MySimpleTarget;
+
+/**
+ * Created by monk on 2018/3/27.
+ */
+
+public class GccImageLoader implements IZoomMediaLoader {
+    @Override
+    public void displayImage(@NonNull Fragment context, @NonNull String path, @NonNull final MySimpleTarget<Bitmap> simpleTarget) {
+
+        RequestOptions options = new RequestOptions()
+                .placeholder(R.drawable.ic_launcher)// 正在加载中的图片
+                .error(R.drawable.ic_launcher).diskCacheStrategy(DiskCacheStrategy.ALL);  // 加载失败的图片
+
+        Glide.with(context).applyDefaultRequestOptions(options).load(path).into(new SimpleTarget<Drawable>() {
+            @Override
+            public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+                BitmapDrawable bd = (BitmapDrawable) resource;
+                Bitmap bm = bd.getBitmap();
+                simpleTarget.onResourceReady(bm);
+            }
+
+            @Override
+            public void onLoadStarted(@Nullable Drawable placeholder) {
+                super.onLoadStarted(placeholder);
+                simpleTarget.onLoadStarted();
+            }
+
+            @Override
+            public void onLoadFailed(@Nullable Drawable errorDrawable) {
+                super.onLoadFailed(errorDrawable);
+                simpleTarget.onLoadFailed(errorDrawable);
+            }
+        });
+
+
+
+    }
+
+
+    @Override
+    public void onStop(@NonNull Fragment context) {
+        Glide.with(context).onStop();
+    }
+
+    @Override
+    public void clearMemory(@NonNull Context c) {
+        Glide.get(c).clearMemory();
+    }
+}
